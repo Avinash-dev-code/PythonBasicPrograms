1b7e7770c40c02a4409e68d3ee71cab0ba1bb5ee
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-12 10:19:32 +0530

    operator programm added

---
+++ Operators.py
@@ -0,0 +1,26 @@
+#This programm contains show you all operators for understanding.
+#Already Defined variable value
+a=23
+b=54
+print(a+b) #Addition operator
+print(a-b) #Substraction operator
+print(a/b) #Division operator
+print(a*b) #Multiplication operator
+print(a%b) #Modules operator    
+print(a//b)#Integer division operator
+print(a**b)#Exponent
+print("\n")
+print("Below enter a  two number numbers")
+#Take input value from user
+c=int(input()) #if you do not cast or declar any data type like int,decimal and all then by default they accept as a string.
+d=int(input()) 
+print("This is your first number:", c)
+print("This is your second number:", d)
+
+print(c+d)
+print(c-d)
+print(c/d)
+print(c*d)
+print(c%d)
+print(c//d)
+print(c**d)
